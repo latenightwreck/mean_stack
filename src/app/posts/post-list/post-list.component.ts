43bbfc0e2c68d17f9c45e21e024ceb5643f76ce9
Morgan Blaze <morgan.b.armstrong@gmail.com>
2018-09-28 23:37:14 -0400

    Created header and post list component. stylized post-create component

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+  posts = [
+    // { title: 'First Post', content: 'This is the content of the first post' },
+    // { title: 'Second Post', content: 'This is the content of the second post' },
+    // { title: 'Third Post', content: 'This is the content of the third post' }
+  ];
+}
