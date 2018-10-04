049823bdaa9b0bfce5f7cbbb11d09d67763eee03
Morgan Blaze <morgan.b.armstrong@gmail.com>
2018-10-03 22:58:51 -0400

    call backend to get list of posts

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,31 @@
 const express = require('express');
 
 const app = express();
-
 app.use((req, res, next) => {
-  console.log('First middleware');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 })
 
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '33432143',
+      title: 'First Server side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: '4543645',
+      title: 'Second Server side post',
+      content: 'This is coming from the server too!'
+    },
+  ]
+  res.status(200).json(posts)
 })
 
 module.exports = app;
