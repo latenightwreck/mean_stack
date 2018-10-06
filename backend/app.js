404c1a76e6bbbd477cf7079ee30da739127b29cd
Morgan Blaze <morgan.b.armstrong@gmail.com>
2018-10-06 14:34:16 -0400

    add put methods to the header

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS');
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
 })
 
@@ -32,6 +32,20 @@ app.post('/api/posts', async (req, res, next) => {
     message: "This is a message!",
     post: post
   });
+})
+
+app.put('/api/posts/:id', async (req, res, next) => {
+  const post = await Post.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  console.log(post);
+
+  res.status(200).json({
+    message: 'Update successful'
+  })
+
 })
 
 app.delete('/api/posts/:id', async (req, res, next) => {
