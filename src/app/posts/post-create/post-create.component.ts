b7742b77f2070451e588817cb6a92bef4ea70fe3
latenightwreck <morgan.b.armstrong@gmail.com>
2018-10-08 17:09:20 -0400

    Allow for only valid file types to be uploaded (jpg, jpeg, png)

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -53,7 +54,8 @@ export class PostCreateComponent implements OnInit {
           validators: Validators.required
         }),
         image: new FormControl(null, {
-          validators: Validators.required
+          validators: Validators.required,
+          asyncValidators: [mimeType]
         })
       });
       if (paramMap.has('postId')) {
