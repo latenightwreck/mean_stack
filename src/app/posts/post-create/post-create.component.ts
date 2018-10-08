be3af47da0b0e092260fc2a127747e97327fb8c5
Morgan Blaze <morgan.b.armstrong@gmail.com>
2018-10-07 23:10:45 -0400

    When loading data, show progress wheel. Centered on screen

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -15,12 +15,13 @@ export class PostCreateComponent implements OnInit {
   private mode = 'create';
   private postId: string;
   post: Post;
+  isLoading = false;
 
   onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-
+    this.isLoading = true;
     if (this.mode === 'create') {
       this.postsService.addPost(form.value.title, form.value.content);
       form.resetForm();
@@ -34,7 +35,9 @@ export class PostCreateComponent implements OnInit {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
+        this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
           this.post = postData;
         });
       } else {
